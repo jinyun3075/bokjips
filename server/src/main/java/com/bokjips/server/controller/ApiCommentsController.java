6a41be8c2b21d1b1a34b86ca0906bf21bf960ce6
jinyunjae <jinyun3075@naver.com>
2022-06-07 14:45:28 +0900

    :sparkles: 댓글 리스트

--- server/src/main/java/com/bokjips/server/controller/ApiCommentsController.java
+++ server/src/main/java/com/bokjips/server/controller/ApiCommentsController.java
@@ -2,15 +2,14 @@ package com.bokjips.server.controller;
 
 import com.bokjips.server.domain.comments.dto.CommentsRequestDto;
 import com.bokjips.server.domain.comments.dto.CommentsResponseDto;
+import com.bokjips.server.domain.comments.entity.Comments;
 import com.bokjips.server.service.CommentsService;
+import com.bokjips.server.util.dto.PageResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Log4j2
@@ -24,4 +23,11 @@ public class ApiCommentsController {
     public ResponseEntity<CommentsResponseDto> insertComments(@RequestBody CommentsRequestDto dto) throws Exception {
         return new ResponseEntity<>(commentsService.insertComments(dto), HttpStatus.OK);
     }
+
+    @GetMapping("/select/{corp_id}")
+    public ResponseEntity<PageResponseDto<CommentsResponseDto,Comments>> selectListComments(@PathVariable String corp_id,@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size) throws Exception{
+        return new ResponseEntity<>(commentsService.selectListComments(corp_id, page, size),HttpStatus.OK);
+    }
+
+
 }
--- server/src/main/java/com/bokjips/server/domain/comments/repository/CommentsRepository.java
+++ server/src/main/java/com/bokjips/server/domain/comments/repository/CommentsRepository.java
@@ -1,7 +1,10 @@
 package com.bokjips.server.domain.comments.repository;
 
 import com.bokjips.server.domain.comments.entity.Comments;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentsRepository extends JpaRepository<Comments, String> {
+    Page<Comments> findByCorpId(String corp_id, Pageable pageable);
 }
